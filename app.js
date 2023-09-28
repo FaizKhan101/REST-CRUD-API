a9d2622a54f79cc63cf94ea2f3f104c7c6d609a2
Faiz <faizkhan8225@gmail.com>
2023-09-28 19:18:53 +0530

    Add routes for get todos and post todo and epxress.json() middleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = requir("express")
+const express = require("express")
 
 const db = require("./data/database")
+const todosRoutes = require("./routes/todos.routes")
 
 const app = express()
 
+app.use(express.json())
+
+app.use("/todos", todosRoutes)
 
 app.use((error, req, res, next) => {
     res.status(500).json({
